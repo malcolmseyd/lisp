3d8af0bba40e8befe37fd71481391b5f9f5f0a41
Malcolm Seyd <malcolmseyd@gmail.com>
2022-04-04 21:23:19 -0700

    add let form

--- prelude.lisp
+++ prelude.lisp
@@ -3,6 +3,24 @@
 (defmacro not (a)
   (if a nil #t))
 
+;; (let ((a 1))
+;;   body)
+;;
+;; ((lambda (a)
+;;    body)
+;;  1)
+;; TODO write quasiquote like seriously this is gross
+(defmacro let (bindings body)
+  (if bindings
+      (list ; application
+       (list ; lambda expression
+        'lambda ; form name
+        (list (car (car bindings))) ; parameter (a)
+        (eval ; body (recurse to look for more lambdas)
+         (list 'let (cdr bindings) 'body))) 
+       (car (cdr (car bindings)))) ; applied parameter (1)
+      body)) ; recursion bottom case
+
 (define map
   (lambda (f ls)
     (if ls
